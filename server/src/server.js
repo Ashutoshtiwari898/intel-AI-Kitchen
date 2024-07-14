14721aef8553f0847436eca20d6cb913e28646ac
Jaideep Bose <jaideepbose51@gmail.com>
2024-07-14 16:43:29 +0530

    added benchmark

--- server/src/server.js
+++ server/src/server.js
@@ -12,19 +12,19 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000; // specify port name
 
-
-
 // Enable cors for all routes
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 
-
 app.use("/user", userRouter);
 app.use("/kitchen", kitchenRouter);
 
-
-
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack); // log error stack to console
+    res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
